3c446baeca808be4349e483bba845440e33bbc0f
Mateusz Walendzik <mate.walendzik@gmail.com>
2020-03-16 21:46:35 +0100

    small refactor

--- src/representation/UnitFactory.ts
+++ src/representation/UnitFactory.ts
@@ -1,16 +1,13 @@
 import Unit from './Unit'
 import createSoliderSprite from '~/sprites/soliderSprites'
-import createMySelectionSprite from '~/sprites/mySelectionSprite'
 import { ModelDetails } from '~/sprites/types'
 
 class UnitsFactory {
   private static getSoliderSprite: () => ModelDetails
-  private static getMySelection: () => PIXI.Sprite
   private static layerGroup: PIXI.display.Group
 
   static initializationTypes(layerGroup: PIXI.display.Group) {
     this.getSoliderSprite = createSoliderSprite()
-    this.getMySelection = createMySelectionSprite()
     this.layerGroup = layerGroup
   }
 
@@ -18,7 +15,6 @@ class UnitsFactory {
     const graphicParams = {
       parentGroup: this.layerGroup,
       unit: new PIXI.Container(),
-      selection: this.getMySelection(),
     }
 
     return new Unit(x, y, angle, graphicParams, this.getSoliderSprite())
---
+++ src/representation/getSprites.ts
@@ -0,0 +1,98 @@
+const getFormattedNumber = (value: number) => {
+  const stringifiedValue = `${value}`
+  return new Array(5 - stringifiedValue.length).join('0') + stringifiedValue
+}
+
+const addFrames = (
+  frames: PIXI.Texture[],
+  numberOfIteration: number,
+  getTextureName: (value: string) => string,
+) => {
+  for (let i = 0; i < numberOfIteration; i++) {
+    const formattedNumber = getFormattedNumber(i)
+    const textureName = getTextureName(formattedNumber)
+    frames.push(PIXI.Texture.from(textureName))
+  }
+}
+
+export default () => {
+  const frames = []
+
+  // [{ sides: 12, lenght: 6}].map((item, index, arr) => ({
+  //   ...item,
+  //   first:
+  //   last:
+  // }))
+
+  const framesPeriods = {
+    STAY: {
+      first: 0,
+      sides: 12,
+      length: 1,
+      last: 11,
+    },
+    SHOOT: {
+      first: 12,
+      sides: 12,
+      length: 6,
+      last: 12 * 6 + 12 - 1,
+    },
+    GO: {
+      first: 72 + 12,
+      sides: 12,
+      length: 16,
+      last: 12 * 16 + 72 + 12 - 1,
+    },
+    FLY: {
+      first: 192 + 72 + 12,
+      sides: 8,
+      length: 31,
+      last: 8 * 31 + 192 + 72 + 12 - 1,
+    },
+    GETUP: {
+      first: 248 + 192 + 72 + 12,
+      sides: 8,
+      length: 24,
+      last: 8 * 24 + 248 + 192 + 72 + 12 - 1,
+    },
+  }
+
+  addFrames(
+    frames,
+    framesPeriods.STAY.sides * framesPeriods.STAY.length,
+    id => `idle_${id}_solider_run${id}.png.png`,
+  )
+
+  addFrames(
+    frames,
+    framesPeriods.SHOOT.sides * framesPeriods.SHOOT.length,
+    id => `_${id}_s_f${id}.png.png`,
+  )
+
+  addFrames(
+    frames,
+    framesPeriods.GO.sides * framesPeriods.GO.length,
+    id => `_${id}_s_g${id}.png.png`,
+  )
+
+  addFrames(
+    frames,
+    framesPeriods.FLY.sides * framesPeriods.FLY.length,
+    id => `_${id}_s_h${id}.png.png`,
+  )
+
+  addFrames(
+    frames,
+    framesPeriods.GETUP.sides * framesPeriods.GETUP.length,
+    id => `_${id}_s_gu${id}.png.png`,
+  )
+
+  return (): PIXI.AnimatedSprite => {
+    const movieClip = new PIXI.AnimatedSprite(frames)
+    movieClip.animationSpeed = 0.4
+    movieClip.scale.set(0.7)
+    // movieClip.anchor.set(0.5, 0);
+
+    return movieClip
+  }
+}
