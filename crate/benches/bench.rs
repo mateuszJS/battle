aa28a1471507ff8f28e3bedb0e82228bb9bd5a4e
mateuszJS <mate.walendzik@gmail.com>
2020-05-26 17:00:08 +0200

    update name in bench

--- crate/benches/bench.rs
+++ crate/benches/bench.rs
@@ -8,20 +8,20 @@ to do bench you have to
 
 #![feature(test)]
 
-extern crate rust_webpack_template;
+extern crate Battle;
 extern crate test;
 
 use test::Bencher;
 
 #[bench]
 fn bench_find_track_floats32(b: &mut Bencher) {
-  rust_webpack_template::position_utils::obstacles_lazy_statics::ObstaclesLazyStatics::init_and_get_obstacles_handler(Some(vec![
+  Battle::position_utils::obstacles_lazy_statics::ObstaclesLazyStatics::init_and_get_obstacles_handler(Some(vec![
     600.0, 100.0, 900.0, 100.0, 900.0, 300.0, 600.0, 300.0, // end here
     700.0, 400.0, 900.0, 400.0, 900.0, 600.0, 700.0, 600.0, 600.0, 500.0,
   ]));
   // let n = test::black_box(1000);
-  rust_webpack_template::position_utils::PositionUtils::get_track(100.0, 100.0, 950.0, 450.0);
+  Battle::position_utils::PositionUtils::get_track(100.0, 100.0, 950.0, 450.0);
   b.iter(|| {
-    rust_webpack_template::position_utils::PositionUtils::get_track(100.0, 100.0, 950.0, 450.0);
+    Battle::position_utils::PositionUtils::get_track(100.0, 100.0, 950.0, 450.0);
   });
 }
