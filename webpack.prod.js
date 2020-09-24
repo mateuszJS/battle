f7b855d3f8dfc6931f3c6ae6ef7005ae6e245f8e
mateuszJS <mate.walendzik@gmail.com>
2020-09-24 15:26:41 +0200

    add webpack copy plugin to copy the assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,12 +4,17 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
 	plugins: [
 		new UglifyJSPlugin(),
-		new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'assets', to: 'assets' },
+      ],
+    })
 		// new BundleAnalyzerPlugin()
 	]
 })
