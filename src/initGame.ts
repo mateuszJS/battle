f27556fc66f5883569453dd19a194206a76ca814
mateuszJS <mate.walendzik@gmail.com>
2021-05-25 14:33:14 +0200

    enhanced animated sprites

--- src/initGame.ts
+++ src/initGame.ts
@@ -13,44 +13,14 @@ import getSerializedInfoAboutWorld from './getSerializedInfoAboutWorld'
 import { createFactoryButtons } from './buttons/factory'
 import REPRESENTATION_IDS from '~/render/representationsIds'
 import debugController from '~/debug'
+import enhanceAnimatedSprites from '~/enhance-animated-sprites'
 
 export type UniverseRepresentation = {
   [id: number]: Factory | Unit | StrategicPoint
 }
 
-interface EnhancedAnimateSprite extends PIXI.AnimatedSprite {
-  prototype: any
-  updateTexture: Function,
-}
-
 const initGame = () => {
-  (PIXI.AnimatedSprite as unknown as EnhancedAnimateSprite).prototype.updateTexture = function() {
-    const currentFrame = this.currentFrame;
-
-    if (this._previousFrame === currentFrame) {
-      return;
-    }
-
-    if (this.onFrameChange) {
-      if (this.onFrameChange(this.currentFrame)) {
-        return // do not change the current frame
-      }
-    }
-
-    this._previousFrame = currentFrame;
-
-    this._texture = this._textures[currentFrame];
-    this._textureID = -1;
-    this._textureTrimmedID = -1;
-    this._cachedTint = 0xFFFFFF;
-    this.uvs = this._texture._uvs.uvsFloat32;
-
-    if (this.updateAnchor) {
-      this._anchor.copyFrom(this._texture.defaultAnchor);
-    }
-  }
-
-
+  enhanceAnimatedSprites()
 
   const mapSprite = createBackgroundTexture()
   getSortableLayer(mapSprite)
--- src/render/index.ts
+++ src/render/index.ts
@@ -12,12 +12,6 @@ const render = (
   delta: number,
   universeData: number[],
   universeRepresentation: UniverseRepresentation,
-  // updateStage: Function,
-  // factories: Factory[],
-  // getUnitType: Function,
-  // updateResCounter: Function,
-  // createEmptyArr: Function,
-  // resourcesPoints: ResPoint[],
 ) => {
   const universeLength = universeData.length
   let factionId
@@ -130,80 +124,6 @@ const render = (
   }
 
   BulletFactory.update()
-
-  // updateStage()
-  // window.flamesUpdaters.forEach(update => update())
-  // window.smokeContainer.elements.forEach(EffectsFactory.updateSmoke)
-  // if (window.timer % 2) {
-  //   factories.forEach((factory, index) => {
-  //     factory.update()
-  //     if (factory.resources >= 1000 && index > 0) {
-  //       factory.buySquad(getUnitType(index))
-  //     }
-  //     if (index === 0) {
-  //       updateResCounter()
-  //       if (factory.resources >= 1000) {
-  //         window.userIcons.forEach(icon => (icon.alpha = 1))
-  //       } else {
-  //         window.userIcons.forEach(icon => (icon.alpha = 0.5))
-  //       }
-  //     }
-  //   })
-  // }
-  // if (window.timer % SETTINGS.CHANGE_STATE_THROTTLE === 0) {
-  //   window.allSquads.forEach(faction =>
-  //     faction.forEach(squad => squad.updateProps()),
-  //   )
-  //   window.squadsWereMoved = createEmptyArr()
-  //   window.allSquads.forEach(faction => {
-  //     faction.forEach(squad => {
-  //       squad.updateStayingStatus()
-  //       if (!squad.isStaying) {
-  //         window.squadsWereMoved[squad.faction].push(squad)
-  //       }
-  //     })
-  //   })
-  //   window.allSquads.forEach(faction => {
-  //     faction.forEach(squad => squad.update())
-  //   })
-  //   //patrz czy jendsotki na siebie w jednym obrocie pętli nie wpływają
-  //   resourcesPoints.forEach(resPoint => resPoint.update())
-  // }
-  // window.allSquads.forEach(faction => {
-  //   faction.forEach(squad => squad.animate())
-  // })
-  // window.bulletContainer.forEach(bullet => bullet.update()) //patrz czy pociski na siebie nie wpływaja w jednym obrocie pętli
-  // if (++window.hutningTimer >= SETTINGS.HUNTING_REFRESH_TIME) {
-  //   window.allSquads.forEach(faction => {
-  //     faction.forEach(squad => squad.regroupMembers())
-  //   })
-  //   manageHunters(window.hunters)
-  //   window.hunters = createEmptyArr()
-  //   window.hutningTimer = 0
-  // }
-  // if (window.timer === 10) {
-  //   const maps = influenceController.calcMapsValues(resourcesPoints)
-  //   //pass maps here to AiController
-  //   window.allSquads.forEach((fac, idx) => {
-  //     if (idx !== 0) {
-  //       aiController.manageFaction(idx, maps, resourcesPoints, factories[idx])
-  //     }
-  //   })
-  // }
-  // window.timer++
-  // if (window.timer > 60) {
-  //   window.timer = 0
-  // }
-  // Icons.showIcons()
-  // window.allSquads.forEach(faction =>
-  //   faction.forEach(squad => {
-  //     squad.abilities.forEach(ability => {
-  //       if (ability.time > 0) {
-  //         ability.time--
-  //       }
-  //     })
-  //   }),
-  // )
 }
 
 export default render
